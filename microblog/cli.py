b80244ec3fa95692d856ae2701578efc322c639b
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-05-27 18:10:01 -0300

    feat: tests with pytest

--- microblog/cli.py
+++ microblog/cli.py
@@ -5,7 +5,7 @@ from sqlmodel import Session, select
 
 from .config import settings
 from .db import engine
-from .models import User, Post
+from .models import User, Post, SQLModel
 from .security import HashedPassword
 
 main = typer.Typer(name="Microblog CLI")
@@ -65,3 +65,14 @@ def create_user(email: str, username: str, password: str):
         session.refresh(user)
         typer.echo(f"created {username} user")
         return user
+
+@main.command()
+def reset_db(
+    force: bool = typer.Option(
+        False, "--force", "-f", help="Run with no confirmation"
+    )
+):
+    """Resets the database tables"""
+    force = force or typer.confirm("Are you sure?")
+    if force:
+        SQLModel.metadata.drop_all(engine)
