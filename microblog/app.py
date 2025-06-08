2620272cc2bf8a2dcfaf2ccd63f33de9adad3659
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-06-08 11:40:23 -0300

    fix: initialize Beanie ODM on FastAPI startup. Add database initialization using FastAPI startup event

--- microblog/app.py
+++ microblog/app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from microblog.routes import main_router
+from microblog.database import init_db
 
 app = FastAPI(
     title="Microblog",
     version="0.0.1",
     description="A simple microblog app",
 )
+
+@app.on_event("startup")
+async def startup_event():
+    await init_db()
 
 @app.get('/')
 async def index():
